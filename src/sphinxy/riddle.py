05f5f1a3095eddb6d4cf6715cea68d60826df3a2
Mariia <romanuik7meri.ua@gmail.com>
2023-04-20 14:26:31 -0600

    Added docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -8,7 +8,20 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Checks the answer.
+
+        Args:
+            answer (str): The given answer to the riddle.
+
+        Returns:
+            bool: The result of the check of the answer.
+        """
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
+        """Get a hint.
+
+        Returns:
+            Iterator: Iterable of strings of the hint to the riddle.
+        """
         yield from iter(self.answer)
